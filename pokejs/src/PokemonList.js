43123c6116d23091d1fbc09ae7473ecdc8515cb2
Açal HABI <ahabi@softia.fr>
2019-11-18 16:21:07 +0100

    list pokemons finished

--- pokejs/src/PokemonList.js
+++ pokejs/src/PokemonList.js
@@ -3,135 +3,189 @@ import MaterialTable from 'material-table';
 
 class PokemonList extends Component
 {
-    static type_list = {
-        'normal': 'normal',
-        'fire': 'fire',
-        'fighting': 'fighting',
-        'water': 'water',
-        'flying': 'flying',
-        'grass': 'grass',
-        'poison': 'poison',
-        'electric': 'electric',
-        'ground': 'ground',
-        'psychic': 'psychic',
-        'rock': 'rock',
-        'ice': 'ice',
-        'bug': 'bug',
-        'dragon': 'dragon',
-        'ghost': 'ghost',
-        'dark': 'dark',
-        'steel': 'steel',
-        'fairy': 'fairy',
-        'unknwon': 'unknwon',
-        'shadow': 'shadow'
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            moveIsLoaded: false,
+            typeIsLoaded: false,
+            move_list: {},
+            type_list: {}
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://pokeapi.co/api/v2/move?offset=0&limit=9999")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                var tmp_moves={};
+                for (let i = 0; i < result.count; i++)
+                    tmp_moves[result.results[i].name] = result.results[i].name;
+                this.setState({
+                    moveIsLoaded: true,
+                    move_list: tmp_moves
+                });
+            },
+            (error) => {
+                this.setState({
+                    moveIsLoaded: true,
+                    error
+                });
+            }
+        )
+        fetch("https://pokeapi.co/api/v2/type")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                var tmp_types={};
+                for (let i = 0; i < result.count; i++)
+                    tmp_types[result.results[i].name] = result.results[i].name;
+                this.setState({
+                    typeIsLoaded: true,
+                    type_list: tmp_types
+                });
+            },
+            (error) => {
+                this.setState({
+                    typeIsLoaded: true,
+                    error
+                });
+            }
+        )
     }
 
     static filters(pokemons, query)
     {
         pokemons = pokemons.filter(poke => poke.name.includes(query.search));
-        if (query.filters.length !== 0) {
-            for (var i = 0; i < query.filters[0].value.length; i++)
-                pokemons = pokemons.filter(poke => poke.type.includes(query.filters[0].value[i]));
+        for (let i = 0; i < query.filters.length; i++) {
+            if (query.filters[i].column.field === "type")
+                for (let j = 0; j < query.filters[i].value.length; j++)
+                    pokemons = pokemons.filter(poke => poke.type.includes(query.filters[i].value[j]));
+            else
+                for (let j = 0; j < query.filters[i].value.length; j++)
+                    pokemons = pokemons.filter(poke => poke.move.includes(query.filters[i].value[j]));
         }
         return pokemons;
     }
 
+    static displayPicsPokemon(rowData) {
+        return <img
+            style={{ height: 50, borderRadius: '50%' }}
+            src={rowData.picture}
+            alt={"no_picture"}
+        />
+    }
+
+    static displayTypes(rowData) {
+        var table = []
+        var types = rowData.type.split(', ');
+
+        for (let i = 0; i < types.length; i++) {
+            table.push(<tr>{types[i]}</tr>)
+        }
+        return table
+    }
+
+    static displayMoves(rowData) {}
+
     render()
     {
-        return (
-            <MaterialTable
-                title="Pokedex"
-                columns={[
-                    { title: 'ID', field: 'id', searchable: false, filtering: false },
-                    { title: 'Picture', field: 'picture', searchable: false, filtering: false, render: rowData => (
-                        <img
-                            style={{ height: 50, borderRadius: '50%' }}
-                            src={rowData.picture}
-                            alt={"no_picture"}
-                        />
-                    )},
-                    { title: 'Name', field: 'name', type: 'string', filtering: false },
-                    { title: 'Type', field: 'type', searchable: false, lookup: PokemonList.type_list, render: (rowData) => {
-                        let table = []
-                        let types = rowData.type.split(', ');
+        const { error, move_list, type_list} = this.state;
+        if (error) {
+            return <div>Erreur : {error.message}</div>;
+        } else {
+            return (
+                <MaterialTable
+                    title="Pokedex"
+                    columns={[
+                        { title: 'ID', field: 'id', searchable: false, filtering: false },
+                        { title: 'Picture', field: 'picture', searchable: false, filtering: false, render: (rowData) => PokemonList.displayPicsPokemon(rowData)},
+                        { title: 'Name', field: 'name', type: 'string', filtering: false },
+                        { title: 'Types', field: 'type', searchable: false, lookup: type_list, render: (rowData) => PokemonList.displayTypes(rowData)},
+                        { title: 'Moves', field: 'move', searchable: false, lookup: move_list, render: (rowData) => PokemonList.displayMoves(rowData)}
 
-                        for (let i = 0; i < types.length; i++) {
-                            table.push(<tr>{types[i]}</tr>)
-                        }
-                        return table
-                    }}
-                ]}
-                data={query =>
-                    new Promise((resolve, reject) => {
-                        let url = 'https://pokeapi.co/api/v2/pokemon?'
-                        url += 'offset=' + (query.page * query.pageSize)
-                        url += '&limit=' + query.pageSize
-                        fetch(url)
-                        .then(response => response.json())
-                        .then(result => {
-                            var i = 0;
-                            var pokemons = [];
-                            var fetchNow = function(i) {
-                                fetch(result.results[i].url)
-                                .then(response => response.json())
-                                .then(result_detail => {
-                                    if (i < result.results.length) {
-                                        let tmp = { id: -666, name: result.results[i].name, type: ['unknown'], picture: 'https://wiki.p-insurgence.com/images/0/09/722.png' }
-                                        tmp.id = result_detail.id;
-                                        tmp.picture = result_detail.sprites.front_default;
-                                        if (result_detail.types.length)
-                                            tmp.type = [];
-                                        for (var j = 0; j < result_detail.types.length; j++)
-                                            tmp.type.push(result_detail.types[j].type.name);
-                                        tmp.type = tmp.type.join(", ");
-                                        pokemons.push(tmp);
-                                        i++;
-                                    }
-                                    if (i === result.results.length) {
-                                        pokemons = PokemonList.filters(pokemons, query);
-                                        resolve({
-                                            data: pokemons,
-                                            page: query.page,
-                                            totalCount: result.count
-                                        })
-                                    }
-                                    else
-                                        fetchNow(i);
-                                })
-                            }
-                            fetchNow(i);
+                    ]}
+                    data={query =>
+                        new Promise((resolve, reject) => {
+                            let url = 'https://pokeapi.co/api/v2/pokemon?'
+                            url += 'offset=' + (query.page * query.pageSize)
+                            url += '&limit=' + query.pageSize
+                            fetch(url)
+                            .then(response => response.json())
+                            .then(result => {
+                                let i = 0;
+                                var pokemons = [];
+                                var fetchNow = function(i) {
+                                    fetch(result.results[i].url)
+                                    .then(response => response.json())
+                                    .then(result_detail => {
+                                        if (i < result.results.length) {
+                                            let tmp = { id: -666, name: result.results[i].name, type: ['unknown'], picture: 'https://wiki.p-insurgence.com/images/0/09/722.png', move: ['no_moves'] }
+                                            tmp.id = result_detail.id;
+                                            tmp.picture = result_detail.sprites.front_default;
+                                            if (result_detail.types.length)
+                                                tmp.type = [];
+                                            if (result_detail.moves.length)
+                                                tmp.move = [];
+                                            for (let j = 0; j < result_detail.types.length; j++)
+                                                tmp.type.push(result_detail.types[j].type.name);
+                                            for (let j = 0; j < result_detail.moves.length; j++)
+                                                tmp.move.push(result_detail.moves[j].move.name);
+                                            tmp.type = tmp.type.join(", ");
+                                            tmp.move = tmp.move.join(", ");
+                                            pokemons.push(tmp);
+                                            i++;
+                                        }
+                                        if (i === result.results.length) {
+                                            pokemons = PokemonList.filters(pokemons, query);
+                                            resolve({
+                                                data: pokemons,
+                                                page: query.page,
+                                                totalCount: result.count
+                                            })
+                                        }
+                                        else
+                                            fetchNow(i);
+                                    })
+                                }
+                                fetchNow(i);
+                            })
                         })
-                    })
-                }
-                options={{
-                    search: true,
-                    filtering: true,
-                    pageSizeOptions: [5, 10, 20, 50],
-                    headerStyle: {
-                      backgroundColor: '#01579b',
-                      color: '#FFF'
                     }
-                }}
-                detailPanel={[{
-                    tooltip: 'Show Name',
-                    render: rowData => {
-                        return (
-                            <div
-                            style={{
-                                fontSize: 100,
-                                textAlign: 'center',
-                                color: 'white',
-                                backgroundColor: '#43A047',
-                            }}
-                            >
-                            {rowData.id}
-                            </div>
-                        )
-                    },
-                }]}
-                onRowClick={(event, rowData, togglePanel) => togglePanel()}
-            />
-        );
+                    options={{
+                        search: true,
+                        filtering: true,
+                        pageSizeOptions: [5, 10, 20, 50],
+                        headerStyle: {
+                          backgroundColor: '#01579b',
+                          color: '#FFF'
+                        },
+                        rowStyle: rowData => ({
+                            backgroundColor: (rowData.tableData.id % 2 === 0) ? '#DDD' : '#FFF'
+                        })
+                    }}
+                    detailPanel={[{
+                        tooltip: 'Show Details',
+                        render: rowData => {
+                            return (
+                                <div
+                                style={{
+                                    fontSize: 100,
+                                    textAlign: 'center',
+                                    color: 'white',
+                                    backgroundColor: '#43A047',
+                                }}
+                                >
+                                {rowData.id}
+                                </div>
+                            )
+                        },
+                    }]}
+                    onRowClick={(event, rowData, togglePanel) => togglePanel()}
+                />
+            );
+        }
     }
 }
 
